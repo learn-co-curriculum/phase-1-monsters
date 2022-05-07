c79dd4311800f920c4a839f6ffa21a4ee6fbbe68
Kevin Nielsen <knielsen24@gmail.com>
2022-05-07 10:47:55 -0700

    2nd commit w/ forward button

--- js/index.js
+++ js/index.js
@@ -1,19 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {   
-    //postMonster()
-    getMonsters()
-
+document.addEventListener('DOMContentLoaded', () => {
+    getMonsters(url, page)
 })
+
+const url = 'http://localhost:3000/monsters'
+let page = 1
 
 const monsterContainer = document.querySelector('#monster-container')
 const divContainer = document.querySelector('#create-monster')
-
-
+const backButton = document.querySelector('#back')
+const forwardButton = document.querySelector('#forward')
 
 const createForm = document.createElement('form')
 const nameForm = document.createElement('input')
-const nameLabel = document.createElement('label')
 const ageForm = document.createElement('input')
-const ageLabel = document.createElement('label')
 const descriptionForm = document.createElement('input')
 const descriptionLabel = document.createElement('label')
 const createButton = document.createElement('button')
@@ -21,25 +20,42 @@ const createButton = document.createElement('button')
 createForm.id = 'monster-form'
 nameForm.id = 'name'
 nameForm.type = 'text'
-nameLabel.htmlFor = 'name'
-nameForm.value = 'name'
+nameForm.placeholder = 'name'
 ageForm.id = 'age'
 ageForm.type = 'text'
-ageLabel.htmlFor = 'age'
-ageForm.value = 'age'
+ageForm.placeholder = 'age'
 descriptionForm.id = 'description'
 descriptionForm.type = 'text'
-descriptionLabel.htmlFor = 'description'
-descriptionForm.value = 'description'
+descriptionForm.placeholder = 'description'
 createButton.type = 'submit'
 createButton.textContent = 'Create'
 
 divContainer.append(createForm)
-createForm.append(
-    nameLabel, nameForm, ageLabel, ageForm, descriptionLabel, descriptionForm, createButton, 
-    )
+createForm.append(nameForm, ageForm, descriptionForm, createButton)
+
+const loadMonsterButtons = button => {
+    button.addEventListener('click', (e) => {
+        let page = 1
+        if (button === forwardButton) {
+            while (page > 0) {
+                return getMonsters(url, (page + 1))
+            }    
+        }
+        // button works but renders the first 50 monsters again.
+        // else if (button ===  backButton) {
+        //     while (page > 0) {
+        //         pageInc = page - 1
+        //         return getMonsters(url, pageInc)
+        //     }    
+        // }
+    })    
+}
+
+loadMonsterButtons(backButton)
+loadMonsterButtons(forwardButton)
 
 const formSelect = document.querySelector('#monster-form')
+//console.log(formSelect)
 
 formSelect.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -50,7 +66,6 @@ formSelect.addEventListener('submit', (e) => {
     }]
     renderMonsters(createMonsterArray)
     postMonster(createMonsterArray)
-
 })
 
 
@@ -68,26 +83,22 @@ const renderMonsters = monstersArray => {
 }
 
 
-
-
-    const getMonsters = () => {
-        fetch('http://localhost:3000/monsters/?_limit=50&_page=1')
-            .then(r => r.json())
-            .then(monstersArray => {
-                //console.log(monstersArray)
-                renderMonsters(monstersArray)
-            })
-    }
+const getMonsters = (url, page) => {
+    fetch(`${url}/?_limit=50&_page=${page}`)
+        .then(r => r.json()).then(monstersArray => {
+            renderMonsters(monstersArray)
+        })
+}
 
 
 const postMonster = (newMonster) => {
     const configObj = {
         method: "POST",
-        headers: { "Content-Type": "application/json", "Accept": "application/json"},
+        headers: { "Content-Type": "application/json", Accept: "application/json"},
         body: JSON.stringify(newMonster),
     }
     
-    fetch("http://localhost:3000/monsters", configObj)
+    fetch(url, configObj)
         .then(r => r.json()).then(data => data).catch(error => {
             alert("new monster not uploaded")
             error.message
